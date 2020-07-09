844db278257ff23130c837155385ce6fb454eae9
RJDan <youniversehq@gmail.com>
2020-07-09 16:02:56 -0400

    guide finally fixed moving on

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,2 @@
 document.getElementById("footer").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " YouniverseHQ";
-
-document.getElementById("pageNav").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='play.html'>Play</a></li>" +
-"<li><a href='guide.html'>guide</a></li>" +
-"</ul>";
